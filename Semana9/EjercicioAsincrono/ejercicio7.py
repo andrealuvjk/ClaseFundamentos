82e2cceb4b27cfdfa71a97a18ff0f2d99c2b1b1c
andrealuvjk <andreaochoa497@gmail.com>
2026-03-21 22:34:49 -0600

    ejercicio asincrono 7 sem9

---
+++ Semana9/EjercicioAsincrono/ejercicio7.py
@@ -0,0 +1,9 @@
+#Solicite al usuario una frase y reemplace todas las apariciones de la palabra "Python" 
+# por "Programación" utilizando el método replace().
+
+frase = input("Escriba una frase que incluya la palabra Python")
+
+reemplazo = frase.replace("Python","programacion")
+
+print("la nueva frase es: ")
+print(reemplazo)
