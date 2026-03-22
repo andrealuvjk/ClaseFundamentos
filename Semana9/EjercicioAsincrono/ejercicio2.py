c93f887eabaa884476c7102321a7f3bf73b2ec00
andrealuvjk <andreaochoa497@gmail.com>
2026-03-21 21:54:05 -0600

    ejercicio asincrono 2 sem9

---
+++ Semana9/EjercicioAsincrono/ejercicio2.py
@@ -0,0 +1,15 @@
+#Cree un programa que solicite una frase al usuario. Luego muestre:
+# La frase original.
+ #La frase en mayúsculas.
+ #La frase en minúsculas.
+
+frase = input("Escriba una frase o palabra: ")
+fraseOriginal = frase
+print("La frase original es: ")
+print(fraseOriginal)
+
+print("Frase en mayusculas: ")
+print(frase.upper())
+
+print("Frase en minuscula")
+print(frase.lower())
