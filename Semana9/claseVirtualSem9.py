a817f51af6f64408482f7edf9049d2ad77da99b9
andrealuvjk <andreaochoa497@gmail.com>
2026-03-26 20:33:40 -0600

    retroalimentacion pt2 sem9

---
+++ Semana9/claseVirtualSem9.py
@@ -0,0 +1,140 @@
+Serie = "fullmetal alchemits"
+
+### cada variable tiene un espacio de memoria asignado
+
+## cuando una vable cambia => se pierde la inmutabilidad
+## POO
+## polimorfismo -> es el cambio de acciones sin que se rompa el codigo
+## abstracciones ->
+##      Tasa de cafe
+#       cafe coscafe
+##       azucar
+##      agua
+##      otros ingredientes
+##  un objeto es el que toma un modelo y este modelo le da funciones y utiliza sus propiedades
+
+## Leon ->
+## tiene ojos  (propiedades)
+## tiene boca
+## Esta guapo
+#############
+#  corre        ( Funciones )
+# salta
+
+## Clases
+## Estructura de datos.
+
+
+## es arreglo es una variable que tiene adentro otra variable
+## Listas.
+## Arrays.-> se inia a contar desde el 0
+## tuplas.
+## indices.
+
+
+# -------------------------------------------------------
+def saludo(nombres):
+    print(nombres)
+
+
+# saludo(Serie) las funciones simpre van  a tener ()
+# -------------------------------------------------------
+## las funciones tienen un espacio
+## Scope es dond reciden las variables
+
+## Colocar el nombre de la serie como titulo
+fmaTemu = Serie.title()
+# saludo(Serie)
+# saludo(fmaTemu)
+fnaMayusculas = Serie.upper()
+saludo(fnaMayusculas)
+## deprogracion Lineal
+FullmetalCapitalizer = fnaMayusculas.swapcase().title()
+## cuando encadenamos funciones se indica que la salidad de la funcion actual
+# 3 es la entrada de la siguiente funcion.
+
+saludo(FullmetalCapitalizer)
+
+## compara cadenas de texo
+comparar1 = "Ever"
+comparar2 = "Ever"
+
+# casefold para comparar y pasar a  minusculas
+variableTemporal = comparar2.casefold()
+comparar = comparar1.casefold() == comparar2.casefold()
+# print(comparar)
+## casefold nos dara true unicamente si los elementos son identicos sino nos indcara un false
+
+## para verificar si es nu nuemero o un caracter vamos a utilizar isalfa()
+clasicas2005 = "Gasolina"
+comprarisAlpha = clasicas2005.isalpha()
+# print(comprarisAlpha, 2005)
+# isalpha nos va a dar true si el string que se le esta enviando es unicamente letras
+
+## si lo que quiero es que sea solo numero  isalnum
+
+letraCancion = " Los que paso, paso, entre tu y yo."  # 3 no tengo numeor
+decada = "10"
+
+ejemplo = letraCancion.isalnum()
+# print(ejemplo)
+ejemplo = decada.isalnum()
+# print(ejemplo)
+## isalnum verifica si la cadena de texto solo tiene numeros si solo tiene numeros nos va a dar tru
+# si etiene u solo espacio dara false
+
+## varificar que solo sean digitos
+comprobarDecadas = decada.isdigit()
+print(comprobarDecadas)
+
+##si la cadena de texto es numerica
+siTieneNumero = "Andrea 2006"
+##este texto tiene un numero -> true
+##isnueric
+respuesta = siTieneNumero.isnumeric()
+print(respuesta)
+
+isLowerCase = "Ella se fue, me abandono y destrozo mi corazon"
+minuscula = isLowerCase.islower()
+print("solo minusculas", minuscula)
+
+## solo debe de tener mayusculas
+fraseIconica = "Soltala Erika"
+respuesta = fraseIconica.isupper()
+##esta funcion se encadenar con otra
+print(respuesta)
+
+## tener un contenido -> entrada a las funcion que inicie con el .
+
+respuesta = Serie.upper().isupper()
+## esta funcion la vamos a encadenar con la otra >:)
+print(respuesta)
+
+respuesta = fraseIconica.title().istitle()
+print(fraseIconica, respuesta)
+## cuando una funcion retorna algo ( tipo de datos)
+## string ->  espacio de memoria diferentes al de un espacio numerico
+## int
+# Float
+## Decimal
+## Boolean
+
+# string  -> E v e r  -> lista o un array
+# variable :
+# tiene un tipo
+# nobre sea unico ->  indice
+# no puede inicar con variables
+# va a tomar siempre el valor de la ultima modificacion
+print(Serie)
+Serie = "Chapulin Colorado"
+print(Serie)
+controlarEspacio = Serie.isspace()
+print(controlarEspacio)
+## islower
+# isupper
+# isspace
+# isalnum
+# isalpha
+
+## Metodos de busqueda
+tema = "En el bosque de china la chinita se perdio  "
