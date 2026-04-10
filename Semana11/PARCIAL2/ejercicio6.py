86f0c2089f28f687aed4c57e6a45776bde8c5bc0
andrealuvjk <andreaochoa497@gmail.com>
2026-04-09 21:02:41 -0600

    resolucion ejercicio6, normalizacion fuerte usando casefold

---
+++ Semana11/PARCIAL2/ejercicio6.py
@@ -0,0 +1,10 @@
+
+texto = "Andrea"
+
+text_normalizado = texto.casefold()
+print(f"texto normalizado: {text_normalizado}")
+
+solo_letras = text_normalizado.isalpha()
+print("el texto tiene solo letras?", solo_letras)
+
+
