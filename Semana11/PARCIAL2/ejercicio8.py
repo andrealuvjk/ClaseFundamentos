25278d795730491eca2021672a972fe9d863cf4a
andrealuvjk <andreaochoa497@gmail.com>
2026-04-09 21:04:32 -0600

    resolucion ejercicio8,dividir poema en saltos de linea y convertirlo a lista

---
+++ Semana11/PARCIAL2/ejercicio8.py
@@ -0,0 +1,13 @@
+
+poema = """Es porque un pajarito de la montaña ha hecho
+en el hueco de un árbol, su nido matinal,
+que el árbol amanece con música en el pecho,"""
+
+print(poema)
+
+contar_a = poema.count("a")
+print("la letra 'a' aparece: ", contar_a , "veces")
+
+dividir_oraciones = poema.splitlines()
+print("\n--lista de oraciones--")
+print(dividir_oraciones)
