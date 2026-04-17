a763f911342f50804c0631a38a3594733e516b85
andrealuvjk <andreaochoa497@gmail.com>
2026-04-17 17:07:41 -0600

    ejercicio 4 asincrono sem12

---
+++ Semana12/ejerciciosAsincronos/ejercicip4.py
@@ -0,0 +1,10 @@
+
+num = int(input("Ingrese un numero para conocer si es par o impar: "))
+
+#para conocer si un numero es par o impar es con su residuo de la division por dos
+#si es igual a 0 es par, si no es impar
+
+if num % 2 == 0:
+    print(f"el numero {num} es par")
+else:
+    print("El numero es impar")
