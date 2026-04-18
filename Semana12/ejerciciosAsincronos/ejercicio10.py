4117c4d6e0cd707f6c4c5d3be70ddf0537f2fcf4
andrealuvjk <andreaochoa497@gmail.com>
2026-04-17 18:05:37 -0600

    ejercicio 10 asincrono sem12

---
+++ Semana12/ejerciciosAsincronos/ejercicio10.py
@@ -0,0 +1,12 @@
+
+user = "andrea"
+pwd = "2006"
+
+usuario = input("Ingrese su usuario: ")
+password = input("Ingrese su contraseña: ")
+
+if user == usuario and pwd == password:
+    print("Acceso permitido")
+else:
+    print("Acceso denegado")
+    
