ca7c395c14582ebbd59d5b4fc7f59ae34f27988f
andrealuvjk <andreaochoa497@gmail.com>
2026-05-14 19:29:37 -0600

    clases semana 15 y 16

---
+++ semana15/array.py
@@ -0,0 +1,24 @@
+series = []
+
+## agregar
+series.append("batman")  # 1.
+series.append("Dr.House")  # 2
+series.append("Super man")  # 3
+series.append("Luthor")  # 4
+series.append("Eren Yeager")  # 5
+series.append("Monkey D. Luffy")  # 6
+series.append("CarMan")  # 7
+series.append("Patrick")  # 8
+
+# para agregar un elemento a un arrays es necesario utilizar appen()
+# seres.appen("")
+# series.insert(1, "Patrick")
+# series.remove("Patrick")
+# series.pop(8)
+series.insert(2, "Deadpool")
+series.insert(1, "Super man")
+series.pop(3)
+series.append("Go Diego Gogo ")
+
+series.insert(0, "Kokun")
+print(series) 
